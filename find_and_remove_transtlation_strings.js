99fe30f05577b189c4a808b779259aba99b21448
Tomas-Bartos <44730661+Tomas-Bartos@users.noreply.github.com>
2025-05-23 12:58:37 +0000

    Find and remove strings, renamed and added description

--- find_and_remove_transtlation_strings.js
+++ find_and_remove_transtlation_strings.js
@@ -1,7 +1,12 @@
+// This function finds and remove all strings created by automatic transtlation by Edge browser.
+// 1. Transtlate your HTML file in new window. 
+// 2. Then run the script in the console
+// 3. Copy and use transtlated HTML
+
 (function removeMicrosoftTranslatorAttributes() {
   const html = document.body.innerHTML;
 
-  // Find all occurencies
+  // Find all occurencies of _msthash="103"; _msttexthash="94913"; _msthidden="3", all numbers in the "" are counted for
   const matches = html.match(/\s*_(?:msthash|msttexthash|msthidden|istranslated)="\d+"/g);
 
   // If found count them and then delete
